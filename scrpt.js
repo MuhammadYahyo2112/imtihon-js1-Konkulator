df2d748881159f0f71aabd5a287ed35f59e257ee
MuhammadYahyo2112 <yahyo0121@gmail.com>
2025-04-26 05:17:36 +0300

    Add files via upload

--- scrpt.js
+++ scrpt.js
@@ -9,42 +9,43 @@
 
   
   const kalkulyator = document.getElementById("kalkulyator");
-  kalkulyator.style.width = "200px";
-  kalkulyator.style.height = "80px";
+  kalkulyator.style.width = "300px";
+  kalkulyator.style.height = "150px";
   kalkulyator.style.backgroundColor = "red";
   kalkulyator.style.color = "cyan"; 
   kalkulyator.style.textAlign = "center";
+  kalkulyator.style.borderRadius = "30px";
   kalkulyator.style.lineHeight = "80px"; 
   kalkulyator.style.margin = "10px";  
   
 
   
   const button = document.getElementById("check");
-  button.style.width = "200px";
-  button.style.height = "80px";
+  button.style.width = "300px";
+  button.style.height = "150px";
   button.style.backgroundColor = "cyan";
   button.style.color = "white"; 
   button.style.fontSize = "18px";
   button.style.border = "none";
   button.style.cursor = "pointer";
-  button.style.borderRadius = "20px";
+  button.style.borderRadius = "30px";
   button.style.marginTop = "20px"; 
 
   
   const natijaDiv = document.getElementById("natija");
-  natijaDiv.style.width = "200px";
-  natijaDiv.style.height = "80px";
+  natijaDiv.style.width = "300px";
+  natijaDiv.style.height = "150px";
   natijaDiv.style.backgroundColor = "blue"; 
   natijaDiv.style.color = "white"; 
   natijaDiv.style.textAlign = "center";
   natijaDiv.style.lineHeight = "80px";
   natijaDiv.style.marginTop = "20px";
-  natijaDiv.style.borderRadius = "20px";
+  natijaDiv.style.borderRadius = "30px";
 
 
   let son1 = +prompt("1-sonni kiriting:");
   let son2 = +prompt("2-sonni kiriting:");
-  let amal = prompt("Amalni kiriting: +, -, *, /");
+  let amal = prompt("Amalni kiriting\n +\n -\n *\n /");
 
 
   kalkulyator.textContent = `Kalkulyator: ${son1} ${amal} ${son2}`;
